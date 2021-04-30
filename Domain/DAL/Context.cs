101f01c2a095955b954a64f5b4ef6f99b156785b
PiotrMc <77527394+PiotrMc@users.noreply.github.com>
2021-04-30 21:54:17 +0200

    2 nd

---
+++ Domain/DAL/Context.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Consumer> Customers { get; set; }
+        public Context()
+        {
+            sqlServer = ConfigurationHelper.Configuration.GetConnectionString("SQLServer");
+        }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        private readonly string sqlServer;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (sqlServer != null)
+            {
+                optionsBuilder.UseSqlServer(sqlServer);
+            }
+        }
+
+    }
+}
---
+++ Domain/Models/Consumer.cs
@@ -0,0 +1,20 @@
+﻿using EntityHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Consumer : Entity
+    {
+        public String CustomerFirstName { get; set; }
+        public String CustomerLastName { get; set; }
+        public String CustomerEmail { get; set; }
+        [Column(TypeName = "Date")]
+        public DateTime CustomerDateofBirth { get; set; }
+
+    }
+}
